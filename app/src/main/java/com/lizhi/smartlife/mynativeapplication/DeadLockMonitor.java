6ca137f7058550a4a1dbf56f5b93b840f92967cb
lanxiaobin <lanxiaobin@lizhi.com>
2021-05-14 00:34:31 +0800

    死锁堆栈信息打印

--- app/src/main/java/com/lizhi/smartlife/mynativeapplication/DeadLockMonitor.java
+++ app/src/main/java/com/lizhi/smartlife/mynativeapplication/DeadLockMonitor.java
@@ -3,6 +3,9 @@ package com.lizhi.smartlife.mynativeapplication;
 import android.os.Build;
 import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -78,6 +81,7 @@ public class DeadLockMonitor {
     void startMonitor() {
         int nativeInitResult = nativeInit(Build.VERSION.SDK_INT);
         Log.i(TAG, "nativeInit: " + nativeInitResult);
+        HashMap<Integer, DeadLockThread> deadLock = new HashMap<>();
 
         Thread[] threads = getAllThreads();
         for (Thread thread : threads) {
@@ -97,14 +101,101 @@ public class DeadLockMonitor {
                 if (blockThreadId != 0 && curThreadId != 0) {
                     Log.w(TAG, "blockThread = " + blockThreadId + ",curThreadId=" + curThreadId);
 
-                    //todo 最后一步是判断哪个线程造成死锁
-//                    deadLock.put(curThreadId, new DeadLockThread(curThreadId, blockThreadId, thread));
+                    //todo 最后一步是判断哪个线程造成死锁 1->2->3->1  1->2->1
+                    deadLock.put(curThreadId, new DeadLockThread(curThreadId, blockThreadId, thread));
                 }
             }
         }
+
+        // 将所有情况进行分组
+        ArrayList<HashMap<Integer, Thread>> deadLockThreadGroup = deadLockThreadGroup(deadLock);
+        for (HashMap<Integer, Thread> group : deadLockThreadGroup) {
+
+            for (Integer curId : group.keySet()) {
+
+                DeadLockThread deadLockThread = deadLock.get(curId);
+                if (deadLockThread == null) {
+                    continue;
+                }
+
+                Thread waitThread = group.get(deadLockThread.blockThreadId);
+                Thread deadThread = group.get(deadLockThread.curThreadId);
+                if (waitThread == null) {
+                    continue;
+                }
+
+                //输出
+                Log.e(TAG, "startMonitor: waitThread.Name = " + waitThread.getName());
+                Log.e(TAG, "startMonitor: deadThread.Name = " + deadThread.getName());
+
+                StringBuffer sb = new StringBuffer("\r\n");
+                StackTraceElement[] stackTrace = deadThread.getStackTrace();
+                for (StackTraceElement stackTraceElement : stackTrace) {
+                    sb.append(stackTraceElement.toString());
+                    sb.append("\r\n");
+                }
+                Log.e(TAG, sb.toString());
+
+            }
+
+        }
+    }
+
+    private ArrayList<HashMap<Integer, Thread>> deadLockThreadGroup(HashMap<Integer, DeadLockThread> deadLock) {
+
+        //排重而已？
+        HashSet<Integer> hashSet = new HashSet<>();
+
+        ArrayList<HashMap<Integer, Thread>> lockThreadGroup = new ArrayList<>();
+        for (Integer currentThreadId : deadLock.keySet()) {
+            if (hashSet.contains(currentThreadId)) {
+                continue;
+            }
+            hashSet.add(currentThreadId);
+            HashMap<Integer, Thread> deadLockGroup = findDeadLockGroup(currentThreadId, deadLock, new HashMap<Integer, Thread>());
+
+            hashSet.addAll(deadLockGroup.keySet());
+            lockThreadGroup.add(deadLockGroup);
+        }
+
+
+        return lockThreadGroup;
+    }
+
+    private HashMap<Integer, Thread> findDeadLockGroup(Integer currentThreadId, HashMap<Integer, DeadLockThread> deadLock, HashMap<Integer, Thread> threadHashMap) {
+
+        DeadLockThread deadLockThread = deadLock.get(currentThreadId);
+        if (deadLockThread == null) {
+            return new HashMap<>();
+        }
+
+        //已经走了一轮，例如 1->2 然后 2->1
+        if (threadHashMap.containsKey(currentThreadId)) {
+            return threadHashMap;
+        }
+
+        threadHashMap.put(currentThreadId, deadLockThread.thread);
+
+        // 1->2->3->1   123为一组 3和1为这一组
+
+        return findDeadLockGroup(deadLockThread.blockThreadId, deadLock, threadHashMap);
+    }
+
+    class DeadLockThread {
+        public DeadLockThread(int curThreadId, int blockThreadId, Thread thread) {
+            this.curThreadId = curThreadId;
+            this.blockThreadId = blockThreadId;
+            this.thread = thread;
+        }
+
+        int curThreadId;
+        int blockThreadId;
+        Thread thread;
     }
 
     public native int nativeInit(int sdkVersion);
+
     public native int getContentThreadIdArt(long threadAddress);
+
     public native int getThreadIdFromThreadPtr(long threadAddress);
 }
